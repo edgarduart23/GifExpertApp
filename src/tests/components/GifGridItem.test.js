693da57c8a1e6221dc0cb62f7ed63ee9bbabe8c6
edgardo <nergumero2017@gmail.com>
2020-07-22 18:39:00 -0300

    pruebas del componente

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,9 +3,30 @@ import {shallow} from 'enzyme';
 import {GifGridItem} from '../../components/GifGridItem';
 
 
+const title = 'un titulo';
+const url = 'https://localhost/algo.jpg';
+const wrapper = shallow( <GifGridItem title= {title} url= {url} />)
+
 describe('Pruebas en <GifGridItem />', () => {
+    
     test('debe de mostrar el componente correctamente', () =>{
-        const wrapper = shallow( <GifGridItem />)
+        
         expect(wrapper).toMatchSnapshot();
     })
+    test('debe de tener un párrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect( p.text().trim()).toBe(title);
+    })
+    test('debe de tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect( img.prop('src')).toBe(url);
+        expect( img.prop('alt')).toBe(title);
+    });
+    test('debe de tener animate__fadeOutDown ', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeOutDown')).toBe(true);
+        
+    })
+    
 })
